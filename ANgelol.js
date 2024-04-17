6ec3432165704b45cbe553ac8f4b46ae281fda78
Angelolmega <144715133+Angelolmega@users.noreply.github.com>
2024-04-17 12:08:45 -0400

    Create ANgelol.js

---
+++ ANgelol.js
@@ -0,0 +1,10 @@
+//Loader
+window.addEventListene('load',() => {
+  const loader= document.querySelector('.loader');
+  setTimeout(() => {
+    loader.setAttribute('style', 'opacity: 0;');
+  }, 500);
+  setTimeout(() => {
+    loader.parentNode.removeChild(loader);
+    document.body.style.overflow = 'initial';
+  }, 1000);
